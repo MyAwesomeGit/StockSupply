c9060de8f377bb01cd733fddbe47052fedde095a
Vasily Shuklin <b88bb@yandex.ru>
2025-05-15 19:23:19 +0500

    Initial Commit

---
+++ StockSupply/StockSupplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockSupplyApp.swift
+//  StockSupply
+//
+//  Created by Vasily Shuklin on 15.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct StockSupplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
